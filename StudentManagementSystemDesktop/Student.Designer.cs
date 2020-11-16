0682a46f77bd49e5e448753549f6831d23b6428a
sohailamjad865 <sohailamjad865@gmail.com>
2020-11-16 15:24:36 +0500

    Add Button

--- StudentManagementSystemDesktop/Student.Designer.cs
+++ StudentManagementSystemDesktop/Student.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -40,11 +41,21 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Here Is Student Portal";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Student
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Student";
             this.Text = "Student";
@@ -56,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
